fc5b7a262fefd4ed7fa545afb2d7dfcce0474c41
Ranieri <57837805+xRanierix@users.noreply.github.com>
2020-09-24 21:41:56 -0300

    Terminamos o Pdf 05-02

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'usjt-ccp1bua-data-biding';
+ nome: string;
+ exibeCaixa: boolean = false;
+
+ alterarNome (nome): void{
+   console.log(nome.target.value);
+   this.nome=nome.target.value;
+ }
+
+  lancarDado(): number{
+    return Math.floor(Math.random() * 6) +1;
+  }
+
+  adicionar (nomeInput): void{
+    console.log(nomeInput.value);
+    this.nome = nomeInput.value;
+    this.exibeCaixa = true;
+    // console.log("Adicionando...")
+  }
+
+
 }
